f203644533f310cdf3893a9fa72a02ee8697836b
Bach Ngo <thanbaiks@gmail.com>
2026-01-03 20:20:55 +0700

    refactor: migrate ESLint config from CommonJS to JSON

--- Dockerfile
+++ Dockerfile
@@ -12,4 +12,3 @@ COPY --from=builder /builder/dist .
 COPY Caddyfile /etc/caddy/Caddyfile
 EXPOSE 80
 CMD ["caddy", "run", "--config", "/etc/caddy/Caddyfile"]
-
--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000,
-  },
-  resolve: {
-    alias: {
-      "@": "/src",
+    plugins: [react()],
+    server: {
+        port: 3000,
     },
-  },
+    resolve: {
+        alias: {
+            "@": "/src",
+        },
+    },
 });
